abbee51d1676321acb3384ab895766390b2cc1aa
Vivek Wadher <122199360+vivekuw@users.noreply.github.com>
2025-07-02 16:19:33 +0530

    commit test updated

---
+++ tests/tests_operations.py
@@ -0,0 +1,12 @@
+from github_actions.src.main import add,sub
+
+def test_add():
+    assert add(5,3)==8
+    assert add(2,3)==5
+
+def test_sub():
+    assert sub(5,1)==4
+    assert sub(4,3)==1
+    assert sub(2,3)==-1
+
+
